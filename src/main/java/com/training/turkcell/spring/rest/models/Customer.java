88a905f91ca02d67dcef117a3ce0cfcc3c840541
osman yaycioglu <osman.yaycioglu@gmail.com>
2023-12-08 13:43:48 +0300

    day 3 3

--- src/main/java/com/training/turkcell/spring/rest/models/Customer.java
+++ src/main/java/com/training/turkcell/spring/rest/models/Customer.java
@@ -1,5 +1,6 @@
 package com.training.turkcell.spring.rest.models;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Customer {
-    private String name;
-    private String surname;
+    @NotEmpty
+    @NotBlank
+    @Size(min = 2, max = 15, message = "isim {min} ile {max} arasında olmalı")
+    private String  name;
+    @NotEmpty
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String  surname;
+    @Min(50)
+    @Max(300)
     private Integer height;
+    @Min(10)
+    @Max(400)
     private Integer weight;
 }
